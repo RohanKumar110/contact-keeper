9ea12dc887ee998d776798ba3632480e688215b7
Rohan Kumar <rohanktalreja110@gmail.com>
2021-07-26 22:22:08 +0500

    contactState initial state and provider

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,19 +3,22 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./layout/Navbar";
 import React, { Fragment } from "react";
+import ContactState from "../context/contact/ContactState";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Fragment>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-        </Switch>
-      </div>
-    </Fragment>
+    <ContactState>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </ContactState>
   );
 }
 
