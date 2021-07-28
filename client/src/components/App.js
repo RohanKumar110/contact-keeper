aa67ad6cbe54dbeb8cf6d08fc80acd27e8464605
Rohan Kumar <rohanktalreja110@gmail.com>
2021-07-28 13:42:36 +0500

    add auth context & initial state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,22 +3,25 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./layout/Navbar";
 import React, { Fragment } from "react";
+import AuthState from "../context/auth/AuthState";
 import ContactState from "../context/contact/ContactState";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <ContactState>
-      <Fragment>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </ContactState>
+    </AuthState>
   );
 }
 
